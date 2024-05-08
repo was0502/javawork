d81f3096284cdc795efa80e397de34207822ef67
was0502 <was0502@naver.com>
2024-05-08 14:14:42 +0900

    kurasama

--- day01/src/_03_scanner/T02_Ex01.java
+++ day01/src/_03_scanner/T02_Ex01.java
@@ -4,21 +4,20 @@ public class T02_Ex01 {
 
 	public static void main(String[] args) {
 		// 국어, 컴퓨터, 수학 점수를 입력받아 저장하고
+		// 총점과 평균을 출력하세요
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("국어 점수를 입력하세요 : ");
-		int score = scan.next(); // 문자열의 단어
-		System.out.println("국어 점수 " + score);
+		System.out.print("국어 점수 입력 : ");
+		int kor = sc.nextInt();
+		System.out.print("컴퓨터 점수 입력 : ");
+		int com = sc.nextInt();
+		System.out.print("수학 점수 입력 : ");
+		int math = sc.nextInt();
 		
-		System.out.print("컴퓨터 점수를 입력하세요 : ");
-		int score = scan.nextInt();
-		System.out.println("컴퓨터 점수 : " + score);
-		
-		System.out.print("수학 점수를 입력하세요 : ");
-		double score = scan.nextDouble();
-		System.out.println("수학 점수"+ score);
+		int sum = kor + com + math;
+		System.out.println("총점 : " + sum);
+		System.out.println("평균 : " + sum/3);
 
-		// 총점과 평균을 출력하세요.
-	    System.out.printf("총점은 %d 이고, 점수 평균은 %f입니다");
 	}
 
 }

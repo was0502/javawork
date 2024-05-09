3873e19feb0be7ab55c5bcaba30473402d80091c
was0502 <was0502@naver.com>
2024-05-09 18:33:39 +0900

    kurasama

--- day03/src/_02_conditional/T03_multi_if.java
+++ day03/src/_02_conditional/T03_multi_if.java
@@ -6,10 +6,12 @@ public class T03_multi_if {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("100 : ");
+		System.out.println("점수 입력 : ");
 		int score = sc.nextInt();
 		
-		if(score >= 90)
+		if(score >= 100 || score < 0)
+			System.out.println("점수를 잘못입력하였습니다");
+		else if(score >= 90)
 			System.out.println("A학점");
 		else if(score >= 80)
 			System.out.println("B학점");
@@ -18,7 +20,9 @@ public class T03_multi_if {
 		else if(score >= 60)
 			System.out.println("D학점");
 		else if(score >= 50)
-			System.out.println("E학점");
+			System.out.println("F학점");
+		else 
+			System.out.println("점수는 음수가 될 수 없습니다.");
 		
 
 	}
